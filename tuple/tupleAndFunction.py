582a9f0d595a8d64d48f7160ba2bbe9b2fe53978
Qamar Uddin <uddin.qamar@outlook.com>
2026-03-19 11:40:41 +0200

    Update tupleAndFunction.py

--- tuple/tupleAndFunction.py
+++ tuple/tupleAndFunction.py
@@ -1,11 +1,12 @@
 # tuple is a type of data structure
-# a tuple is a litbit different from a list, it cant be changed or updated once it has been created
+# a tuple is a bit different from a list, it cant be changed or updated once it has been created
 coordinates = (2,4,66,33,77,22,10,100)
 print(coordinates)
 print(coordinates[2])
 
 
 #Function
+# Example 1:
 def say_hi():
     print("hello dear")
     x = 'Welcome to the python course'
@@ -16,11 +17,15 @@ def say_hi():
     print(w)
 say_hi()
 
-def user_inf(name,age,city):
-    print("Hello " + name + " And you are " + str(age) + " year old, and you are from " + city)
+# Example 2:
+def user_inf(name, age, city):
+    print("My name is " + name +! " I am  " + str(age) + " year old, and I from " + city)
 user_inf("Qamar", 27, "Helsinki")
 user_inf("Zaman", 17, "Karachi")
+
 # use of return key words.
+# Example 3:
+
 def cube(num):
     print(num)
     return num*num*num
